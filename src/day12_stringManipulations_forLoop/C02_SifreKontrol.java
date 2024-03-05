eb52a2c42b7dd285c6257a5d5cf6d06ad6aece3e
bulutluoz <abbulutluoz@gmail.com>
2024-03-05 19:24:21 +0100

    day12 sonu

---
+++ src/day12_stringManipulations_forLoop/C02_SifreKontrol.java
@@ -0,0 +1,80 @@
+package day12_stringManipulations_forLoop;
+
+import java.util.Scanner;
+
+public class C02_SifreKontrol {
+
+    public static void main(String[] args) {
+        //Soru 4 : Kullanicidan bir sifre isteyip,
+        //         asagidaki sartlari kontrol edin
+        //         ve kullaniciya duzeltmesi gereken tum eksikleri soyleyin,
+        //         eger tum sartlari saglarsa, "sifre basariyla kaydedildi" yazdirin
+        //         - ilk harf kucuk harf olmali
+        //         - son karakter rakam olmali
+        //         - sifre bosluk icermemeli
+        //         - uzunlugu en az 10 karakter olmali
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen sifrenizi giriniz...");
+        String sifre = scanner.nextLine();
+
+        /*
+            eger if-else kullanirsaniz
+
+            + tum hatalari sirasiyla kontrol eder,
+              en sonda hata if'lerine takilmazsa basarili sifre yazdirabiliriz
+            - if else cumlelerinde, suzgec mantigi oldugundan
+              sadece 1 hatayi yazdirabiliriz
+
+            Bagimsiz if cumleleri kullanirsak
+            + sifredeki tum hatalari yazdirabiliriz
+            - bagimsiz if cumleleri kodun kendileri disindaki kismiyla ilgilenmez
+              yani sifrenin basarili oldugunu yazdirabilmek icin
+              ekstra bir kontrol mekanizmasi kullanmalisiniz
+         */
+
+        int flag = 20;
+
+
+        //         - ilk harf kucuk harf olmali
+        char ilkHarf = sifre.charAt(0);
+        if ( !Character.isLowerCase(ilkHarf) ){
+            System.out.println("Ilk karakter kucuk harf olmali");
+            flag = 15;// hata varsa hep ayni degeri atayacagiz
+        }
+
+        //         - son karakter rakam olmali
+
+        char sonHarf = sifre.charAt(sifre.length()-1);
+        if ( !(sonHarf>='0' && sonHarf<='9')){
+
+            System.out.println("Son karakter rakam olmali");
+            flag = 15;
+        }
+
+        //         - sifre bosluk icermemeli
+
+        if (sifre.contains(" ")){
+            System.out.println("Sifre bosluk icermemeli");
+            flag = 15;
+        }
+
+        //         - uzunlugu en az 10 karakter olmali
+
+        if (sifre.length() < 10){
+            System.out.println("Sifrenin uzunlugu en az 10 karakter olmali");
+            flag = 15;
+        }
+
+
+        // flag kullanarak sonuc yazdirma
+
+        if (flag == 20){
+            System.out.println("Sifre basarili olarak kaydedildi");
+        }else {
+            System.out.println("Lutfen hatalari duzelterek yeniden sifre giriniz");
+        }
+
+
+    }
+}
