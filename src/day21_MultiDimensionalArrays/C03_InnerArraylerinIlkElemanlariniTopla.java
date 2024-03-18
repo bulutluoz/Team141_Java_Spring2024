67ea873fb33d157366a3492355fb049259b4fc42
bulutluoz <abbulutluoz@gmail.com>
2024-03-18 20:16:23 +0100

    day21 sonu

---
+++ src/day21_MultiDimensionalArrays/C03_InnerArraylerinIlkElemanlariniTopla.java
@@ -0,0 +1,29 @@
+package day21_MultiDimensionalArrays;
+
+public class C03_InnerArraylerinIlkElemanlariniTopla {
+
+    public static void main(String[] args) {
+
+        //Soru 1- Verilen 2 katli bir array’de
+        //       her bir inner array'deki 0. index'de bulunan sayilari toplayip,
+        //       sonucu yazdiran bir method olusturun.
+
+        int[][] arr = {{3,6},{1,6,9},{8,0,2,1},{4}};
+
+        ilkElemanlarinToplami(arr); // Ilk elementlerin toplami : 16
+    }
+
+
+    public static void ilkElemanlarinToplami( int[][] arr ){
+
+        int toplam = 0;
+
+        for (int i = 0; i < arr.length ; i++) {
+
+            toplam += arr[i][0];
+        }
+
+        System.out.println("Ilk elementlerin toplami : " + toplam);
+    }
+
+}
---
+++ src/day21_MultiDimensionalArrays/C04_SonElementlerinToplami.java
@@ -0,0 +1,21 @@
+package day21_MultiDimensionalArrays;
+
+public class C04_SonElementlerinToplami {
+
+    public static void main(String[] args) {
+
+        //Soru 5- Verilen 2 katli bir array’de
+        //        her bir inner array’in son elementlerinin toplaminini
+        //        yazdiran bir method olusturun.
+
+        int[][] arr = {{3,6},{1,6,9},{8,0,2,1},{4}};
+
+
+    }
+
+    public static void sonElementleritopla(int[][] arr){
+
+
+
+    }
+}
