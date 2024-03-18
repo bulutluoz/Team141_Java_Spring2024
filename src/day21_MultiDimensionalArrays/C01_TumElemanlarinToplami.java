8f41426504a3f4a47e84c3cb79bf668e6deeee12
bulutluoz <abbulutluoz@gmail.com>
2024-03-18 20:05:42 +0100

    day01 sonu

---
+++ src/day21_MultiDimensionalArrays/C01_TumElemanlarinToplami.java
@@ -0,0 +1,33 @@
+package day21_MultiDimensionalArrays;
+
+public class C01_TumElemanlarinToplami {
+
+    public static void main(String[] args) {
+
+        // Verilen iki katli integer bir array'deki
+        // tum sayilarin toplamini yazdiran bir method olusturun
+
+        int[][] arr = {{3,6},{1,6,9},{8,0,2,1},{4}};
+
+        elemanlarinToplaminiYazdir(arr); // Verilen array'deki elementlerin toplami : 40
+    }
+
+    public static void elemanlarinToplaminiYazdir(int[][] arr){
+
+        // Eger hic bir sart olmaksizin
+        // 2 katli array'deki tum elementlere ulasmak isterseniz
+        // 2 katli nested bir loop olusturmaliyiz
+        int toplam =0;
+
+        for (int i = 0; i < arr.length ; i++) { // outer arr'yi kontrol eder
+
+            for (int j = 0; j < arr[i].length ; j++) { // herbir inner array'i kontrol eder
+
+                toplam += arr[i][j];
+            }
+        }
+
+        System.out.println("Verilen array'deki elementlerin toplami : " + toplam);
+
+    }
+}
---
+++ src/day21_MultiDimensionalArrays/C02_CiftSayilarinToplami.java
@@ -0,0 +1,34 @@
+package day21_MultiDimensionalArrays;
+
+public class C02_CiftSayilarinToplami {
+
+    public static void main(String[] args) {
+
+        //Soru 1- Verilen 2 katli bir array’de bulunan cift sayilari toplayip,
+        //        sonucu yazdiran bir method olusturun.
+
+        int[][] arr = {{3,6},{1,6,9},{8,0,2,1},{4}};
+        ciftsayilariTopla(arr);
+    }
+
+
+    public static void ciftsayilariTopla(int[][] arr ){
+
+
+        int toplam = 0;
+
+        for (int i = 0; i < arr.length ; i++) { // outer array
+
+            for (int j = 0; j <arr[i].length ; j++) { // inner array
+
+                if ( arr[i][j] % 2 == 0 ){
+                    toplam += arr[i][j];
+                }
+
+            }
+
+        }
+        System.out.println("Array'deki cift sayilarin toplami : " + toplam);
+        // Array'deki cift sayilarin toplami : 26
+    }
+}
