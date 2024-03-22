69d035847b61429b6355c13d8f4b25da667f3f79
bulutluoz <abbulutluoz@gmail.com>
2024-03-22 21:20:18 +0100

    day25 sonu

---
+++ src/day25_constructor/C03_Car.java
@@ -0,0 +1,23 @@
+package day25_constructor;
+
+public class C03_Car {
+
+
+    String marka = "Marka belirtilmedi";
+    String model = "Model belirtilmedi";
+    int yil = 1900;
+    int fiyat;
+
+    C03_Car(){
+
+        marka = "Audi";
+        model = "A3";
+    }
+
+
+    int C03_Car(){ // method
+
+        return 0;
+    }
+
+}
---
+++ src/day25_constructor/C04_CarRunner.java
@@ -0,0 +1,14 @@
+package day25_constructor;
+
+public class C04_CarRunner {
+
+
+    public static void main(String[] args) {
+
+        C03_Car car1 = new C03_Car();
+
+
+    }
+
+
+}
