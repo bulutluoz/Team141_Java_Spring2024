0f9f7b94bf13b867218c692c6f148e68fb16531a
bulutluoz <abbulutluoz@gmail.com>
2024-03-22 20:11:18 +0100

    day25 sonu

--- src/day24_ArrayList_ForEachLoop/C07_OrtakElemanlar.java
+++ src/day24_ArrayList_ForEachLoop/C07_OrtakElemanlar.java
@@ -19,7 +19,19 @@ public class C07_OrtakElemanlar {
         // diger array'deki tum elemanlarla karsilastirmak icin
         // nested loop kullanmak zorundayiz
 
+        for(int each1 : array1){
 
+            for(int each2 : array2){
+
+                if (each1 == each2 && !ortakElemanlarListesi.contains(each1)){
+                    ortakElemanlarListesi.add(each1);
+                }
+
+            }
+
+        }
+
+        System.out.println("ortak elemanlar listesi : " + ortakElemanlarListesi);
 
 
     }
