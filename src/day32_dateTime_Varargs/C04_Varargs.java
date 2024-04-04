75247acb352c642b7a639a3eacb0c377398dc7c5
bulutluoz <abbulutluoz@gmail.com>
2024-04-04 20:19:31 +0200

    day33 sonu

--- src/day32_dateTime_Varargs/C04_Varargs.java
+++ src/day32_dateTime_Varargs/C04_Varargs.java
@@ -8,16 +8,23 @@ public class C04_Varargs {
         // ilki haric geriye kalanlari toplayip
         // ilk sayi ile bu toplami carpip yazdiran bir method olusturun
 
-        islem(2);
-        islem(2,3,4);
-        islem(2,4,5,6,7);
-        islem(0,2,3,4,5,6,7,8,9,5);
+        islem(2); // 0
+        islem(2,3,4); // 14
+        islem(2,4,5,6,7); // 44
+        islem(0,2,3,4,5,6,7,8,9,5); // 0
 
     }
 
     public static void islem(int ilkSayi, int... kalanSayilar){
 
+        int toplam = 0;
 
+        for (int each : kalanSayilar
+             ) {
+            toplam += each;
+        }
+
+        System.out.println("Islem sonucu : " + toplam*ilkSayi);
     }
 
 }
---
+++ src/day33_varargs_stringBuilder/C01_VarargsDikkatEdilecekler.java
@@ -0,0 +1,34 @@
+package day33_varargs_stringBuilder;
+
+public class C01_VarargsDikkatEdilecekler {
+
+    public static void main(String[] args) {
+
+        // verilen farkli adetlerdeki sayilarin
+        // sonuncusu haric geriye kalanlari toplayip
+        // son sayi ile bu toplami carpip, sonucu yazdiran bir method olusturun
+
+       //  islem(3,4,5,6,7,9);
+
+
+        /*
+
+            ONEMLI NOT :
+
+            Varargs'in kapasite sinirlamasi yoktur
+            verilen her sayiyi almak isteyecektir
+            Eger parametre olarak yazilan varars'dan sonra baska bir parametre yazarsaniz
+            varargs kendinden sonraki parametreye HIC BIR ELEMENT birakmaz
+            bu durumda method calismaz
+
+
+            KURAL 1 - Varargs her zaman son parametre olarak yazilmalidir
+            KURAL 2 - kural1'den dolayi, bir method'da birden fazla VARARGS parametre OLAMAZ
+         */
+    }
+
+
+   // public static void islem(int... bastakiSayilar, int sonSayi){
+
+   //}
+}
