f3440c014d2b13925ad68887739d03ade481c498
bulutluoz <abbulutluoz@gmail.com>
2024-04-08 21:19:18 +0200

    day35 sonu

--- src/day36_encapsulation_inheritance/C02_FarkliPackageChildOlmayanClass.java
+++ src/day36_encapsulation_inheritance/C02_FarkliPackageChildOlmayanClass.java
@@ -1,19 +1,29 @@
 package day36_encapsulation_inheritance;
 
+import day14_methodOlusturmaVeKullanma.C04_Palindrome;
 import day35_accessModifiers.C02_Depo;
 
 public class C02_FarkliPackageChildOlmayanClass {
 
     public static void main(String[] args) {
+        // static class uyelerini classIsmi.staticClassUyesiIsmi;
+
         //System.out.println(C02_Depo.privateStr);
         //System.out.println(C02_Depo.defaultStr);
         //System.out.println(C02_Depo.protectedStr);
         System.out.println(C02_Depo.publicStr);
 
         C02_Depo depo = new C02_Depo();
+        // static olmayan class uyelerini objeIsmi.staticOlmayanClassUyesiIsmi;
+
         //System.out.println(depo.privateSayi);
         //System.out.println(depo.defaultSayi);
         //System.out.println(depo.protectedSayi);
         System.out.println(depo.publicSayi);
+
+        System.out.println(C04_Palindrome.isPalindrome("Java Candir")); // false
+
+
     }
+
 }
