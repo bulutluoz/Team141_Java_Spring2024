cbf4a9f0672c71564a87d8d9724b969eb39fba86
bulutluoz <abbulutluoz@gmail.com>
2024-04-18 20:31:50 +0200

    day39 sonu

--- src/day39_inheritance_overriding/C03_AvciKuslar.java
+++ src/day39_inheritance_overriding/C03_AvciKuslar.java
@@ -1,5 +1,8 @@
 package day39_inheritance_overriding;
 
+import day38_inheritance.C04_Kuslar;
+import day38_inheritance.C05_AvciKuslar;
+
 public class C03_AvciKuslar extends C02_Kuslar{
     public void hareket() {
         System.out.println("ucarlar");
@@ -38,11 +41,20 @@ public class C03_AvciKuslar extends C02_Kuslar{
         aramaya data turu secilen class'dan baslariz
         aranan ozellik bulununca,
         hemen ilk bulunani kullanmayiz,
-        buldugumuz method, constructor'in oldugu cl;ass'a kadar
+        buldugumuz method, constructor'in oldugu class'a kadar
         ovverride edilmisse,
         daha guncel bilgiyi kullaniriz
 
+        Farkli bir bakis acisiyla soylersek;
+        EGER constructor ve data turu ayni class'dan ise
+        ozelliklerin variable veya method olmasi farketmez
+        o class'dan aramaya baslar, ilk buldugumuzu kullaniriz
 
+        EGER constructor ve data turu farkli ise
+        variable ve method'lar farkli davranir
+        ikisinde de aramaya data turunden baslariz,
+        variable ilk buldugunu kullanir,
+        method ise bulduktan sonra constructor'a kadar daha gunceli var mi diye bakar
          */
 
         C03_AvciKuslar kartalAvK = new C03_AvciKuslar();
@@ -73,5 +85,69 @@ public class C03_AvciKuslar extends C02_Kuslar{
         //kartalKus.pence(); // CTE kuslar veya hayvanlarda yok
         kartalKus.gaga(); // AvK sivri gagalidir
 
+
+
+        C01_Hayvanlar kartalHayvan = new C03_AvciKuslar();
+
+        kartalHayvan.omur(); // H yasar ve olurler
+
+        kartalHayvan.solunum(); // K akcigerle nefes alir
+        kartalHayvan.cogalma(); // K yumurtayla cogalir
+        //kartalHayvan.kanat(); // CTE hayvanlarda yok
+
+        kartalHayvan.hareket(); // AvK ucarlar
+        kartalHayvan.beslenme(); // AvK etle beslenir
+        //kartalHayvan.pence(); // CTE hayvanlarda yok
+        //kartalHayvan.gaga(); // CTE hayvanlarda yok
+
+
+
+        C02_Kuslar kus1 = new C02_Kuslar();
+        kus1.omur(); // H yasar ve olurler
+
+        kus1.solunum(); // K akcigerle nefes alir
+        kus1.cogalma(); // K yumurtayla cogalir
+        kus1.kanat(); // K kanatlidir
+
+        kus1.hareket(); // H hareket eder
+        kus1.beslenme(); // H beslenir
+        // kus1.pence(); // CTE kuslar veya hayvanlarda yok
+        kus1.gaga(); // K gagalidir
+
+
+
+        C01_Hayvanlar kusHayvanlar = new C02_Kuslar();
+        kusHayvanlar.omur(); // H yasar ve olurler
+
+        kusHayvanlar.solunum(); // K akcigerle nefes alir
+        kusHayvanlar.cogalma(); // K yumurtayla cogalir
+        //kusHayvanlar.kanat(); // CTE hayvanlarda yok
+
+        kusHayvanlar.hareket(); // H hareket eder
+        kusHayvanlar.beslenme(); // H beslenir
+        //kusHayvanlar.pence(); // CTE hayvanlarda yok
+        // kusHayvanlar.gaga(); // CTE hayvanlarda yok
+
+
+        C01_Hayvanlar hayvan1 = new C01_Hayvanlar();
+        hayvan1.omur(); // H yasar ve olurler
+
+        hayvan1.solunum(); // H nefes alir
+        hayvan1.cogalma(); // H  cogalir
+        //hayvan1.kanat(); // CTE hayvanlarda yok
+
+        hayvan1.hareket(); // H hareket eder
+        hayvan1.beslenme(); // H beslenir
+        //hayvan1.pence(); // CTE hayvanlarda yok
+        //hayvan1.gaga(); // CTE hayvanlarda yok
+
     }
 }
+
+
+
+
+
+
+
+
