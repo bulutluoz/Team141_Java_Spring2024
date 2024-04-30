0c925784617fd45525908b53b5b54ee627541347
bulutluoz <abbulutluoz@gmail.com>
2024-04-30 20:45:54 +0200

    day46 sonu

---
+++ src/day46_collections/C05_LinkedList.java
@@ -0,0 +1,17 @@
+package day46_collections;
+
+import java.util.LinkedList;
+
+public class C05_LinkedList {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> isimler = new LinkedList<>();
+
+        // data turu olarak 3 interface'i implement eden
+        // LinkedList sectigimiz icin
+        // List, Queue ve Deque interface'lerinde olan tum method'lari implement etmistir
+
+
+    }
+}
