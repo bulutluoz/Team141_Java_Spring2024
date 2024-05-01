42c37af2502dd9aa7af3cd914505e3c6ffc4ac59
bulutluoz <abbulutluoz@gmail.com>
2024-05-01 21:50:55 +0200

    day47 sonu

---
+++ src/day47_maps/C06_Odev.java
@@ -0,0 +1,15 @@
+package day47_maps;
+
+public class C06_Odev {
+
+    /*
+        1- Ogrenci numarasi olarak verilen min ve max (sinirlar dahil) numaralar
+           arasindaki tum ogrencilerin numara, isim, soyisim, sinif ve subelerini yazdirin
+
+        2- verilen bolumdeki tum ogrencilerin
+           isim soyisim sinif ve subelerini yazdirin
+
+        3- Verilen isimdeki tum ogrencilerin
+           no, soyisim, sinif ve subelerini yazdirsin
+     */
+}
