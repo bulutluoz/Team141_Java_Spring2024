d09134b0ae5256443be4879bbdede0f0691c8a29
bulutluoz <abbulutluoz@gmail.com>
2024-05-07 20:20:13 +0200

    day49 sonu

--- src/day49_NestedMap/C02_NestedOgrenciMapiOlusturma.java
+++ src/day49_NestedMap/C02_NestedOgrenciMapiOlusturma.java
@@ -33,7 +33,77 @@ public class C02_NestedOgrenciMapiOlusturma {
         ogrenciNestedMap.put(101,ogrenci101ValueMap);
 
 
+        System.out.println(ogrenciNestedMap); // {101={sinif=11, sube=H, soyisim=Can, bolum=MF, isim=Ali}}
+
+        mapeOgrenciEkle(102,"Veli","Cem","10","K","TM");
+
+        mapeOgrenciEkle(103,"Ali","Cem","11","K","TM");
+
+        System.out.println(ogrenciNestedMap);
+
+        /*
+                {   101={sinif=11, sube=H, soyisim=Can, bolum=MF, isim=Ali},
+                    102={sinif=10, sube=K, soyisim=Cem, bolum=TM, isim=Veli},
+                    103={sinif=11, sube=K, soyisim=Cem, bolum=TM, isim=Ali}
+                    }
+         */
+
+        // 101 numarali ogrencinin soyismini yazdirin
+
+        // ogrenciNestedMap.get(101) // {sinif=11, sube=H, soyisim=Can, bolum=MF, isim=Ali}
+
+        System.out.println(ogrenciNestedMap.get(101).get("soyisim")); // Can
+
+        // 103 numarali ogrencinin bolumunu yazdirin
+
+        // ogrenciNestedMap.get(103);  // {sinif=11, sube=K, soyisim=Cem, bolum=TM, isim=Ali}
+
+        System.out.println(ogrenciNestedMap.get(103).get("bolum")); // TM
+
+
+        // 102 numarali ogrencinin sinifini 12 yapin
+
+        // ogrenciNestedMap.get(102) // {sinif=10, sube=K, soyisim=Cem, bolum=TM, isim=Veli}
+
+        ogrenciNestedMap.get(102).put("sinif","12");
+
+        System.out.println(ogrenciNestedMap);
+
+        //{101={sinif=11, sube=H, soyisim=Can, bolum=MF, isim=Ali}, 102={sinif=12, sube=K, soyisim=Cem, bolum=TM, isim=Veli}, 103={sinif=11, sube=K, soyisim=Cem, bolum=TM, isim=Ali}}
+
+
+        // Eger 101 numarali ogrencinin sinifi 11 ise, 12 yapin
+
+        String eskiSinif = ogrenciNestedMap.get(101)
+                                           .get("sinif");
+
+        if (eskiSinif.equals("11")){
+            // sinifi 12 yapalim
+            ogrenciNestedMap.get(101)
+                            .put("sinif","12");
+        }
+
+
+
     }
 
+
+
+
+
+
+    public static void mapeOgrenciEkle(Integer ogrenciNo,String isim, String soyisim, String sinif, String sube, String bolum){
+
+        Map<String,String> ogrenciValueMap = new HashMap<>();
+        ogrenciValueMap.put("isim",isim);
+        ogrenciValueMap.put("soyisim",soyisim);
+        ogrenciValueMap.put("sinif",sinif);
+        ogrenciValueMap.put("sube",sube);
+        ogrenciValueMap.put("bolum",bolum);
+
+        ogrenciNestedMap.put(ogrenciNo,ogrenciValueMap);
+
+
+    }
 
 }
